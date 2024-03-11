43000fc1af1c2449667199c61d59fa73ac40a86c
Allesson Figueiredo <allfigueiredo@outlook.com>
2024-03-11 15:50:01 -0300

    adding codan mxl42

---
+++ include/cub3D.h
@@ -0,0 +1,14 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+#include "../MLX42/include/MLX42/MLX42.h"
+#define WIDTH 320
+#define HEIGHT 200
+#endif
